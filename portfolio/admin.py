d54437c3ced409b9b4e6b1a6f575d95de38362af
rolando <rolando@kali.odin>
2020-03-30 08:11:18 -0500

    personal

---
+++ portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    readonlyFields = ('created', 'update')
+    readonly_fields = ('created', 'update')
+# Register your models here.
+admin.site.register(Project, ProjectAdmin)
